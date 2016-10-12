a44ee4420ba268f10f9d8ac5b4b64ee2bdefa861
Rajesh Godavarthi <rgodavarthi@hotmail.com>
2016-10-12 15:24:24 -0700

    kkk

---
+++ TestApp/Prayer/Controllers/PrayerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prayer.Controllers
+{
+    public class PrayerController : Controller
+    {
+        // GET: Prayer
+        public ActionResult Default()
+        {
+            return View();
+        }
+    }
+}
